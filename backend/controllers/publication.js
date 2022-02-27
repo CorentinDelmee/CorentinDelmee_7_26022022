73f509a36bb854c5fe9eca22e9c83dfdd30c2ea8
CorentinDelmee <corentinmdelmee@gmail.com>
2022-02-27 16:11:40 +0100

    User middleware

--- backend/controllers/publication.js
+++ backend/controllers/publication.js
@@ -9,7 +9,7 @@ let publication = require("../models/Publication")
 // Créer une publication
 
 exports.createPublication = (req, res, next) => {
-  let post = new publication(req.post.user_id, req.post.content);
+  let post = new publication(req.body.post.user_id, req.body.post.content);
   let sql = "INSERT INTO postes SET ?";
   let query = connexion.query(sql,post, (err, res) => {
     if(err) throw err;
@@ -32,7 +32,7 @@ exports.getAllPublication = (req,res, next) => {
 // Modification d'une publication
 
 exports.modifyPublication = (req,res, next) => {
-  let sql = `UPTDATE postes SET content = "${req.content}" WHERE id = ${req.params.id}`;
+  let sql = `UPTDATE postes SET content = "${req.body.post_modify.content}" WHERE id = ${req.params.id /*req.body.post_modify.post_id*/}`;
   let query = connexion.query(sql, (err, res) => {
     if(err) throw err;
     console.log(res);
@@ -43,7 +43,7 @@ exports.modifyPublication = (req,res, next) => {
 // Supprimer une publication
 
 exports.deletePublication = (req,res, next) =>{
-  let sql = `DELETE FROM postes WHERE id = ${req.params.id}`
+  let sql = `DELETE FROM postes WHERE id = ${req.params.id /*req.body.post_delete.post_id*/}`
   let query = connexion.query(sql, (err, res) => {
     if(err) throw err;
     console.log(res);
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,3 +1,7 @@
+// Appel de connexion mysql
+
+const connexion = require("../env")
+
 // Appel du package bcrypt
 
 const bcrypt = require("bcrypt");
@@ -13,21 +17,48 @@ const User = require("../models/User");
 // Logique métier : User Inscription
 
 exports.signup = (req, res, next) => {
-  bcrypt.hash(req.body.password, 10)
+  bcrypt.hash(req.body.user.passwordhash, 10)
     .then(hash => {
-      const user = new User({
-        email: req.body.email,
-        password: hash
-      });
-      user.save()
-        .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-        .catch(error => res.status(400).json({ error }));
+      let user = new User(req.body.user.nom, req.body.user.prénom, req.body.user.email, hash);
+      let sql = "INSERT INTO utilisateur SET ?"
+      let query = connexion.query(sql, user, (err, res) => {
+        if(err) throw err;
+      console.log(res);
+      console.log("Utilisateur crée");
+      }) 
     })
     .catch(error => res.status(500).json({ error }));
 };
 
 // Logique métier : User Connexion
 
+exports.login = (req,res, next) => {
+  let user_email = req.body.user_login.email;
+  let sql = "SELECT * FROM utilisateur WHERE email = ?"; /*passwordhash*/
+  let query = connexion.query(sql, user_email, (err, res) => {
+    if(err) throw err;
+    if(res.length > 0) {
+      bcrypt.compare(req.body.user_login.password, res.passwordhash)
+        .then(valid => {
+          if (!valid) {
+            return res.status(401).json({ error: 'Mot de passe incorrect !' });
+          }
+          res.status(200).json({
+            userId: res.id,
+            token: jwt.sign(
+              { userId: res.id },
+              '0tzC7I9v1uDBdKUHrilvGCZ69u8bvrO2h5xw939eA0wBP9l7Sh4MJT498IbxBKT7PYg0GubbKFoXLdK5Zb37p69CB9xcF5AiTLNFhOBoK9PW9I0ubWyqDz9YdTrX4m8jGkBam7gv93wO1wevYx3l6Eyyc9dU6pr4hje7WFyoxmmoM2pzaqI9WKvkYaDbaD8JP9rmadmm',
+              { expiresIn: '24h' }
+            )
+          });
+        })
+      .catch(error => res.status(500).json({ error }));
+    }
+  })
+}
+
+
+/*
 exports.login = (req, res, next) => {
   User.findOne({ email: req.body.email })
     .then(user => {
@@ -52,3 +83,4 @@ exports.login = (req, res, next) => {
     })
     .catch(error => res.status(500).json({ error }));
 };
+*/
