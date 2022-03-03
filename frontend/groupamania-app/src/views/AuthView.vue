a722e0eedd23628d0aec310c3d67216dd60dc945
CorentinDelmee <corentinmdelmee@gmail.com>
2022-03-03 18:15:39 +0100

    Import fichier scss

---
+++ frontend/groupamania-app/src/views/AuthView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <h1>Oui</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'AuthView',
+}
+</script>
--- frontend/groupamania-app/src/views/HomeView.vue
+++ frontend/groupamania-app/src/views/HomeView.vue
@@ -1,18 +1,34 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <body> 
+    <div class="main_interface">
+      <TestBase/>
+      <MainNavgation/>
+      <img alt="Vue logo" src="../assets/logo.png">
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
+  </body> 
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import TestBase from '../components/TestBase.vue'
+import HelloWorld from '../components/HelloWorld.vue'
+import MainNavigation from '../components/MainNavigation.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  Components: {
+    MainNavigation,
+    HelloWorld,
+    TestBase
+  },
 }
 </script>
+
+<style scoped lang="scss">
+@import "../sass/utils/variables";
+
+@import "../sass/base/base";
+
+@import "../sass/pages/home";
+</style>
